e398332b03ee97e6ec496861d6149e6dc6216ced
Matthew Pearson <matthew.pearson@joby.aero>
2022-03-24 10:51:56 -0700

    improved commentz

--- problem_7.py
+++ problem_7.py
@@ -23,20 +23,30 @@ print(f"Part 1 solution: {np.abs(data - int(np.median(data))).sum()}")
 # print(f"Part 2 solution: {fuel_cost.min()}")
 
 # elegant O(N) calculus way
-
-# find centroid c that minimizes:
-#     ε = Σ[(x_i + c)(x_i - c + 1) / 2]
+# Unfortunately the cost function for 🦀_i is not continuously differentiable, so let's instead use
+# the equation for the sum of an arithmetic series,
 #
-# δε/δc = 1/2 Σ[c² - (2x_i + 1)c]
-#       = 1/2 Σ[2c - 2x_i - 1]
-#       = 0
-#   2cN = 2 Σ[x_i] - N
-#     c = (2 Σ[x_i] - N) / 2N
+#      ε_i = s(s + 1)/2
+#
+# where
+#        c = the centroid
+#        s = (x_i - c) = the distance the 🦀 boi has to move
+#
+# find centroid c that minimizes:
+#
+#        ε = Σ[(x_i + c)(x_i - c + 1) / 2]
+#
+#    δε/δc = 1/2 Σ[c² - (2x_i + 1)c]
+#          = 1/2 Σ[2c - 2x_i - 1]
+#          = 0
+#      2cN = 2 Σ[x_i] - N
+#        c = (2 Σ[x_i] - N) / 2N
+
 centroid = int(np.round(((2 * (data).sum() - len(data)) / (2 * len(data)))))
 
-# Unfortunately the real cost function is not continuously differentiable, and our s(s + 1)
-# approximation minimum not at s = 0 but at s = -1/2, the nearest integer centroid value is
-# occasionally (when???) too low by 1. Let's just check the next integer up as well.
+# Since our continuous cost function is not symmetrical about s = 0 and actually has a minumum at
+# s = -1/2, the nearest integer centroid value is occasionally (when???) too low by 1.
+# Let's just check the next integer up as well.
 centroid_candidates = np.array([centroid, centroid + 1])
 distances = np.abs(data.reshape(-1, 1) - centroid_candidates)
 fuel_cost = (distances * (distances + 1) // 2).sum(axis=0)
